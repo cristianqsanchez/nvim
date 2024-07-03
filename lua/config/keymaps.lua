d738368fdf9964aff3b1431b89694519311ff8e3
cristianqsanchez <cristianqsdev@gmail.com>
2024-07-02 19:24:21 -0500

    feat: update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,17 +5,22 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = 'Open explorer' })
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 vim.keymap.set("i", "jj", "<Esc>")
-
+vim.keymap.set("n", "J", "mzJ`z")
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
 
 vim.keymap.set('n', '<leader>ca', '<cmd>%bd|e#<CR>', { desc = 'Close all buffers' })
 
