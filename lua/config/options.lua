54a1e139020d4e0fc90cbdb1f7934a38bacdfffe
cristianqsanchez <cristianqsdev@gmail.com>
2024-09-23 23:34:04 -0500

    feat: update config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,9 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
 vim.opt.cursorline = true
 
 vim.opt.shiftwidth = 2
@@ -21,23 +24,28 @@ vim.opt.inccommand = 'split'
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
-vim.highlight.on_yank()
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
 
 vim.opt.updatetime = 50
 
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.opt.foldcolumn = "0"
-vim.opt.foldtext = ""
-vim.opt.foldlevel = 99
-vim.opt.foldlevelstart = 1
-vim.opt.foldnestmax = 4
+-- vim.opt.foldmethod = "expr"
+-- vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+-- vim.opt.foldcolumn = "0"
+-- vim.opt.foldtext = ""
+-- vim.opt.foldlevel = 99
+-- vim.opt.foldlevelstart = 1
+-- vim.opt.foldnestmax = 4
 
 -- netrw config
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
--- vim.g.netrw_keepdir = 0
 vim.g.netrw_sort_sequence = [[[\/]$,*]]
 vim.g.netrw_bufsettings = 'noma nomod nu rnu nobl nowrap ro'
 
