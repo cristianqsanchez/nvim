5ed02bb1d7959ee606666188578f9310a0a933b0
cristianqsanchez <cristianqsdev@gmail.com>
2025-04-18 23:47:18 -0500

    feat: add quicklist navigation

--- init.lua
+++ init.lua
@@ -3,3 +3,6 @@ require 'core'
 vim.cmd.colorscheme 'oldworld'
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+
+vim.keymap.set('n', '<C-j>', '<cmd>cnext<CR>zz')
+vim.keymap.set('n', '<C-k>', '<cmd>cprev<CR>zz')
